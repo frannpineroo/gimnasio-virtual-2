71ed1909c209eabbe30a7f16cb062cc5a2219ca6
franpi <frannpinero@hotmail.com>
2025-11-26 13:25:57 -0300

    arreglo de la pagina inicial home

--- entidades/views.py
+++ entidades/views.py
@@ -34,7 +34,7 @@ def sign_up(request):
 
 def home_view(request):
     try:
-        return render(request, 'home.html')
+        return render(request, 'entrenador/home.html')
     except TemplateDoesNotExist:
         return HttpResponseServerError("Template entrenador/home.html no encontrado.")
 

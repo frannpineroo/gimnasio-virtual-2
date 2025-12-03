2902816bf94cbc611d8fe5dd5601f2cf45a5d8d9
franpi <frannpinero@hotmail.com>
2025-12-03 09:04:41 -0300

    signin y logout sin errores

--- entidades/views.py
+++ entidades/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponseServerError
 from django.template import TemplateDoesNotExist
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
-from django.contrib.auth import login, logout
+from django.contrib.auth import login, logout, authenticate
 from django.db import IntegrityError
 
 def sign_up(request):
@@ -36,9 +36,20 @@ def signout(request):
     return(redirect('acceso/signin.html'))
 
 def signin(request):
-    return render(request, 'acceso/signin.html', {
-        'form': AuthenticationForm()
-    })
+    if request.method == 'GET':
+        return render(request, 'acceso/signin.html', {
+            'form': AuthenticationForm()
+        })
+    else:
+        user = authenticate(request, username=request.POST['username'], password=request.POST['password'])
+        if user is None:
+            return render(request, 'acceso/signin.html', {
+                'form': AuthenticationForm(),
+                'error': 'El nombre de usuario o la contraseña son incorrectos.'
+            })
+        else:
+            login(request, user)
+            return redirect('home')
 
 def home_view(request):
     try:

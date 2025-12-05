a51bfde3052e9da98f0667117f864374854b5067
franpi <frannpinero@hotmail.com>
2025-12-05 18:44:03 -0300

    arreglo el formulario de crear nueva rutina y crear nuevo ejercicio

--- entidades/forms.py
+++ entidades/forms.py
@@ -6,6 +6,12 @@ class ExerciseForm(ModelForm):
     class Meta:
         model = Exercise
         fields = ['name', 'description', 'muscle_group', 'muscle_subgroup']
+        labels = {
+            'name': 'Nombre del ejercicio',
+            'description': 'Descripción del ejercicio',
+            'muscle_group': 'Grupo muscular',
+            'muscle_subgroup': 'Subgrupo muscular',
+        }
 
 class RutineForm(ModelForm):
     exercises = forms.ModelMultipleChoiceField(
@@ -17,3 +23,13 @@ class RutineForm(ModelForm):
     class Meta:
         model = Rutine
         fields = ['name', 'description', 'time_week', 'days_per_week', 'goal', 'coach', 'client', 'is_template']
+        labels = {
+            'name': 'Nombre de la rutina',
+            'description': 'Descripción de la rutina',
+            'time_week': 'Duración semanal',
+            'days_per_week': 'Días por semana',
+            'goal': 'Objetivo',
+            'coach': 'Entrenador',
+            'client': 'Cliente',
+            'is_template': '¿Es una plantilla?',
+        }
